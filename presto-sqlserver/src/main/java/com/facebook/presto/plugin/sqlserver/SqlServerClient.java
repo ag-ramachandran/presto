fb6c85fbf0892dc6ae19e1dbf13cce95d9f1640e
Ramachandran G <ramacg@microsoft.com>
2022-08-11 17:11:03 +0530

    Hardcode schema

--- presto-sqlserver/src/main/java/com/facebook/presto/plugin/sqlserver/SqlServerClient.java
+++ presto-sqlserver/src/main/java/com/facebook/presto/plugin/sqlserver/SqlServerClient.java
@@ -56,6 +56,8 @@ public class SqlServerClient
     @Override
     protected Collection<String> listSchemas(Connection connection)
     {
+        connection.setCatalog("sdktestsdb");
+        //connection.setSchema("sdktestsdb");
         try (ResultSet resultSet = connection.getMetaData().getSchemas()) {
             log.warn("===================================IN RESULT SET===================================" + connection.getCatalog());
             ImmutableSet.Builder<String> schemaNames = ImmutableSet.builder();
